4e75b04319dff9ea0e3a0807b3f13a235777c34d
Sam <samsakhavarz@gmail.com>
2018-12-03 18:15:38 -0500

    got data from api axios

--- src/App.js
+++ src/App.js
@@ -1,7 +1,40 @@
 import React, { Component } from 'react';
 import './App.css';
+import axios from "axios";
+import Qs from 'qs';
 
 class App extends Component {
+  // constructor() {
+  //   super();
+  //   // this.state({
+  //   //   state: ''
+
+  //   // })
+  // }
+
+  componentDidMount() {
+    axios({
+      url: 'http://proxy.hackeryou.com',
+      dataResponse: 'json',
+      paramsSerializer: function (params) {
+        return Qs.stringify(params, { arrayFormat: 'brackets' })
+      },
+      params: {
+        reqUrl: 'https://www.goodreads.com/search/index.xml',        
+        params: {
+          q: 'dan simmons',
+          key: 'dRJuutBqKWVrrJUND8jbmQ'
+        },
+        proxyHeaders: {
+          'header_params': 'value'
+        },
+        xmlToJSON: true
+      }
+    }).then(res => {
+      console.log(res);
+    });
+  }
+
   render() {
     return (
       <div className="App">
@@ -9,7 +42,7 @@ class App extends Component {
         <p> Welcome to Sabrehawk & Baggins</p>
       </div>
     );
-  }
+  };
 }
 
-export default App;
+  export default App;
