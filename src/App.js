a04f50d4ed8767e33cbb7503e7144dd4f18d9b40
MikeyCouture <michelcouture42@gmail.com>
2018-12-04 17:24:36 -0500

    starting on main app component

--- src/App.js
+++ src/App.js
@@ -4,15 +4,17 @@ import axios from "axios";
 import Qs from 'qs';
 
 class App extends Component {
-  // constructor() {
-  //   super();
-  //   // this.state({
-  //   //   state: ''
-
-  //   // })
-  // }
+  constructor() {
+    super();
+    this.state = ({
+      authorSearch: "",
+      authorWorks: []
+    })
+  }
 
   componentDidMount() {
+
+    // this is the second acios call to GoodReads API - getting the book ID
     axios({
       url: 'http://proxy.hackeryou.com',
       dataResponse: 'json',
@@ -20,10 +22,10 @@ class App extends Component {
         return Qs.stringify(params, { arrayFormat: 'brackets' })
       },
       params: {
-        reqUrl: 'https://www.goodreads.com/search/index.xml',        
+        reqUrl: 'https://www.goodreads.com/book/show',
         params: {
-          q: 'dan simmons',
-          key: 'dRJuutBqKWVrrJUND8jbmQ'
+          key: 'dRJuutBqKWVrrJUND8jbmQ',
+          id: 77566
         },
         proxyHeaders: {
           'header_params': 'value'
@@ -35,11 +37,51 @@ class App extends Component {
     });
   }
 
+  // handling on change input - updating our state of authorSearch
+  handleChange = (e) => {
+    this.setState({
+      [e.target.id] : e.target.value
+    })
+  }
+
+  handleSubmit = (e) => {
+    e.preventDefault();
+    axios({
+      url: 'http://proxy.hackeryou.com',
+      dataResponse: 'json',
+      paramsSerializer: function (params) {
+        return Qs.stringify(params, { arrayFormat: 'brackets' })
+      },
+      params: {
+        reqUrl: 'https://www.goodreads.com/search/index.xml',
+        params: {
+          q: this.state.authorSearch,
+          key: 'dRJuutBqKWVrrJUND8jbmQ'
+        },
+        proxyHeaders: {
+          'header_params': 'value'
+        },
+        xmlToJSON: true
+      }
+    }).then(res => {
+      // filter results to get just an array of author's works, from which we will get all other data
+      this.setState({
+        authorWorks: res.data.GoodreadsResponse.search.results.work
+      })
+      console.log("this is authorWorks",this.state.authorWorks);
+    });
+  }
+
   render() {
     return (
       <div className="App">
         <h1>When You Peaked</h1>
         <p> Welcome to Sabrehawk & Baggins</p>
+        <form onSubmit={this.handleSubmit} action="">
+          <label htmlFor="text">Enter Author Name: </label>
+          <input onChange={this.handleChange} value={this.state.authorSearch} type="text" id="authorSearch" />
+          <input type="submit" value="search" id="#" />
+        </form>
       </div>
     );
   };
