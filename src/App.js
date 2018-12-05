ee5b0cfa999f464fe71943984401ac47bce0f2fc
clairevalant <valantclaire@gmail.com>
2018-12-05 18:34:49 -0500

    all info from second API call sending to state weeeee

--- src/App.js
+++ src/App.js
@@ -15,7 +15,8 @@ class App extends Component {
         year: 0,
         description: "",
         avgRating: 0,
-        cover: ""
+        cover: "",
+        url: ""
       },
       lowBook: {
         id: 0,
@@ -23,7 +24,8 @@ class App extends Component {
         year: 0,
         description: "",
         avgRating: 0,
-        cover: ""
+        cover: "",
+        url: ""
       }
     })
   }
@@ -58,64 +60,99 @@ class App extends Component {
         xmlToJSON: true
       }
     }).then(res => {
-      console.log(res);
       
-        const authorWorks = res.data.GoodreadsResponse.search.results.work
+      const authorWorks = res.data.GoodreadsResponse.search.results.work
 
-        // sorts the array by average rating
-        const sorted = authorWorks.sort((a, b) => {
-          return a.average_rating - b.average_rating
-        });
+      // sorts the array by average rating
+      const sorted = authorWorks.sort((a, b) => {
+        return a.average_rating - b.average_rating
+      });
+            
+      // take the lowest (first) and highest (last) rated books and set state
+      this.setState({
+        sortedWorks: sorted,
+      })
 
-        console.log("sorted:",sorted);
-              
-        // take the lowest (first) and highest (last) rated books and set state
-        this.setState({
-          sortedWorks: sorted,
-        })
-
+      // set state from API info
       this.setState(
-        {
+      {
+        highBook: {
+          id: this.state.sortedWorks[this.state.sortedWorks.length - 1].best_book.id["$t"],
+          title: this.state.sortedWorks[this.state.sortedWorks.length - 1].best_book.title,
+          year: this.state.sortedWorks[this.state.sortedWorks.length - 1].original_publication_year["$t"],
+          avgRating: this.state.sortedWorks[this.state.sortedWorks.length - 1].average_rating,
+          cover: this.state.sortedWorks[this.state.sortedWorks.length - 1].best_book.img_url
+        },
+        lowBook: {
+          id: this.state.sortedWorks[0].best_book.id["$t"],
+          title: this.state.sortedWorks[0].best_book.title,
+          year: this.state.sortedWorks[0].original_publication_year["$t"],
+          avgRating: this.state.sortedWorks[0].average_rating,
+          cover: this.state.sortedWorks[0].best_book.img_url
+        }
+      })
+
+      // pass highBook and lowBook to getDescAndUrl for more info
+      this.getDescAndUrl(this.state.highBook);
+      this.getDescAndUrl(this.state.lowBook);
+    })
+  }
+
+// method to get description asnd url from a different API request, called by handleSubmit
+getDescAndUrl = (book) => {
+  
+  // NEXT AXIOS TEST: find book description and url using id we got from other call
+  axios({
+    url: 'https://proxy.hackeryou.com',
+    dataResponse: 'json',
+    paramsSerializer: function (params) {
+      return Qs.stringify(params, { arrayFormat: 'brackets' })
+    },
+    params: {
+      reqUrl: `https://www.goodreads.com/book/show/${book.id}.xml`,
+      params: {
+        key: 'dRJuutBqKWVrrJUND8jbmQ',
+        text_only: false
+      },
+      proxyHeaders: {
+        'header_params': 'value'
+      },
+      xmlToJSON: true
+    }
+  }).then(res => {
+    // take the lowest (first) and highest (last) rated books and set state
+    // console.log("original res:", res);
+
+    const desc = res.data.GoodreadsResponse.book["description"]
+    const link = res.data.GoodreadsResponse.book.url    
+
+    // if the book we pass to getDescAndUrl is highBook, then set the whole state of highBook, else set the state of lowBook
+    // CAN WE DO THIS ANOTHER WAY????
+    book === this.state.highBook ?
+    this.setState({
           highBook: {
             id: this.state.sortedWorks[this.state.sortedWorks.length - 1].best_book.id["$t"],
             title: this.state.sortedWorks[this.state.sortedWorks.length - 1].best_book.title,
             year: this.state.sortedWorks[this.state.sortedWorks.length - 1].original_publication_year["$t"],
             avgRating: this.state.sortedWorks[this.state.sortedWorks.length - 1].average_rating,
-            cover: this.state.sortedWorks[this.state.sortedWorks.length - 1].best_book.img_url
-          },
-          lowBook: {
-            id: this.state.sortedWorks[0].best_book.id["$t"],
-            title: this.state.sortedWorks[0].best_book.title,
-            year: this.state.sortedWorks[0].original_publication_year["$t"],
-            avgRating: this.state.sortedWorks[0].average_rating,
-            cover: this.state.sortedWorks[0].best_book.img_url
+            cover: this.state.sortedWorks[this.state.sortedWorks.length - 1].best_book.img_url,
+            url: link,
+            description: desc
           }
-        })
-
-      // NEXT AXIOS TEST: Book by id
-      axios({
-        url: 'https://proxy.hackeryou.com',
-        dataResponse: 'json',
-        paramsSerializer: function (params) {
-          return Qs.stringify(params, { arrayFormat: 'brackets' })
-        },
-        params: {
-          reqUrl: `https://www.goodreads.com/book/show/${this.state.highBook.id}.xml`,
-          params: {
-            key: 'dRJuutBqKWVrrJUND8jbmQ',
-            text_only: false
-          },
-          proxyHeaders: {
-            'header_params': 'value'
-          },
-          xmlToJSON: true
+    }) :
+    this.setState({
+        lowBook: {
+          id: this.state.sortedWorks[0].best_book.id["$t"],
+          title: this.state.sortedWorks[0].best_book.title,
+          year: this.state.sortedWorks[0].original_publication_year["$t"],
+          avgRating: this.state.sortedWorks[0].average_rating,
+          cover: this.state.sortedWorks[0].best_book.img_url,
+          url: link,
+          description: desc
         }
-      }).then(res => {
-        // take the lowest (first) and highest (last) rated books and set state
-        console.log("this is res from axios 2:", res);
-      })
     })
-  }
+  })
+}
 
   render() {
     return (
@@ -127,6 +164,7 @@ class App extends Component {
           <input onChange={this.handleChange} value={this.state.authorSearch} type="text" id="authorSearch" />
           <input type="submit" value="search" id="#" />
         </form>
+        <p>{`${this.state.highBook.description}`}</p>
       </div>
     );
   };
