c88182e34885df8429ac6cc38e96175c8d275dbe
clairevalant <valantclaire@gmail.com>
2018-12-06 17:04:28 -0500

    TEST with clg

--- src/App.js
+++ src/App.js
@@ -41,8 +41,12 @@ class App extends Component {
     })
   }
 
+  
+
 // the one that works
   handleSubmit = (e) => {
+    console.log("THIS IS THE WRONG VERSION LOL");
+    
     e.preventDefault();
     axios({
       url: 'http://proxy.hackeryou.com',
